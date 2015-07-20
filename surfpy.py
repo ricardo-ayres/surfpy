71e9dd2826a26ad1a7ab05937724fe44fccebea9
ricardo-ayres <ricardo.bosqueiro.ayres@gmail.com>
2015-07-20 19:59:33 -0300

    first commit

---
+++ surfpy.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python2
+import sys
+import subprocess
+import urllib
+
+# Options:
+browser = "firefox" #insert the command for your preferred browser here.
+
+# Search engines:
+## Insert your custom search engines here following the syntax of the other ones. Don't forget the commas.
+available_engines = {
+"ddg":["https://www.duckduckgo.com/?", "q"],
+"yt":["https://www.youtube.com/results?","search_query"],
+"awiki":["https://wiki.archlinux.org/index.php?", "search"],
+"python2":["https://docs.python.org/2/search.html?", "q"],
+"imgur":["https://imgur.com/search?", "q"],
+}
+
+# End of options.
+# Now this is doing stuff.
+
+if len(sys.argv) < 3:
+    help()
+    sys.exit()
+
+search_engine = sys.argv[1]
+
+if search_engine in available_engines:
+    engine_url = available_engines[search_engine][0]
+    search_prefix = available_engines[search_engine][1]
+
+search_string = " ".join(sys.argv[2::])
+query_url = engine_url + urllib.urlencode({search_prefix:search_string})
+subprocess.Popen([browser, query_url])
+sys.exit(0)
