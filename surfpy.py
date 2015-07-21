ec7e685b1b1cb949d976c4b53356402f0f37a218
ricardo-ayres <ricardo.bosqueiro.ayres@gmail.com>
2015-07-20 22:37:11 -0300

    more tags and better help text

--- surfpy.py
+++ surfpy.py
@@ -5,31 +5,43 @@ import urllib
 
 def print_help():
     global fallback_engine
-    print("Usage: $ surfpy [engine tag] [your search terms]")
+    print("Usage:\n$ surfpy [engine tag] [your search terms]")
+    print("$ surfpy -l (or --list) lists your tags and descriptions and exits.")
+    print("$ surfpy -h (or --help) prints this help and exits.") 
     print("""If the engine tag is not defined in the options all
 the arguments will be passed to a fallback search engine
 defined in the options.""")
-    print("\nCurrent fallback engine:\n%s\t\t%s" % (fallback_engine, available_engines[fallback_engine][2]))
+    print("Current fallback engine:\n%s\t\t%s" % (fallback_engine, available_engines[fallback_engine][2]))
 
 def list_engines():
     for i in available_engines:
-        print("%s\t\t%s" % (i, available_engines[i][2]))
+        if len(i) > 7:
+            print("%s\t%s" % (i, available_engines[i][2]))
+        else:
+            print("%s\t\t%s" % (i, available_engines[i][2]))
 
-# Options:
+##### Options: #####
+
 browser = "firefox" #insert the command for your preferred browser here.
 fallback_engine = "ddg" #set the fallback engine tag you want to use.
+
 # Search engines:
 ## Insert your custom search engines here following the syntax of the other ones. Don't forget the commas.
+# "engine-tag":["engine url with trailing '?' ", " search prefix ", "name or short description"],
+
 available_engines = {
 "ddg":["https://www.duckduckgo.com/?", "q", "DuckDuckGo"],
 "yt":["https://www.youtube.com/results?","search_query", "YouTube"],
 "awiki":["https://wiki.archlinux.org/index.php?", "search", "Arch Wiki"],
 "python2":["https://docs.python.org/2/search.html?", "q", "Python 2.7 documentation"],
 "imgur":["https://imgur.com/search?", "q", "imgur"],
+"wallhaven":["http://alpha.wallhaven.cc/search?", "q", "Wallhaven"],
+"dA":["http://www.deviantart.com/browse/all/?", "section=&global=1&q", "deviantART"],
+"img":["https://duckduckgo.com/?ia=images&iax=1&", "q", "DuckDuckGo image search"],
 }
 
-# End of options.
-# Now this is doing stuff.    
+##### End of options. #####
+   
 if "--list" in sys.argv[:2:] or "-l" in sys.argv[:2:]:
     list_engines()
     sys.exit(0)
