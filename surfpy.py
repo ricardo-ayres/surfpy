2c699973ffd69ab3f06740a91428ef92bff23c83
Ricardo B. Ayres <ricardo.bosqueiro.ayres@gmail.com>
2021-03-04 23:18:03 -0300

    Started rewriting in a sane way

--- surfpy.py
+++ surfpy.py
@@ -1,53 +1,85 @@
-#!/usr/bin/python2
+#!/bin/env python3
 import sys
 import subprocess
-from urllib import urlencode
+from urllib.parse import quote as url_quote
 
 def print_help():
     global fallback_engine
-    print("Usage:\n$ surfpy [engine tag] [your search terms]")
-    print("$ surfpy -l (or --list) lists your tags and descriptions and exits.")
-    print("$ surfpy -h (or --help) prints this help and exits.")
-    print("$ surfpy --print-only will print the string to stdout without passing it to the browser.")
-    print("$ surfpy -b [browser] uses [browser] instead of the browser defined in the options.")
-    print("$ surfpy --dmenu launches dmenu for interactive tag selection.")
-    print("""If the engine tag is not defined in the options all
-the arguments will be passed to a fallback search engine
-defined in the options.""")
-    print("Current fallback engine:\n%s\t\t%s" % (fallback_engine, available_engines[fallback_engine][2]))
+    help_message = "\n".join([
+    "Usage:\n$ surfpy [engine tag] [your search terms]",
+    "$ surfpy -l (or --list) lists your tags and descriptions and exits.",
+    "$ surfpy -h (or --help) prints this help and exits.",
+    "$ surfpy --print-only will print the string to stdout without passing it to the browser.",
+    "$ surfpy -b [browser] uses [browser] instead of the browser defined in the options.",
+    "$ surfpy --dmenu launches dmenu for interactive tag selection.",
+    "If the engine tag is not defined in the options all the arguments will be passed to a fallback search engine defined in the options."
+    ])
 
-def list_engines():
-    for i in available_engines:
-        if len(i) > 7:
-            print("%s\t%s" % (i, available_engines[i][2]))
+    print(
+        "Current fallback engine:\n",
+        f"{sengine.fallback}"
+    )
+
+class sengine:
+    tags = {}
+    fallback = None
+    default_browser = ""
+
+    def __init__(self, tag, url, description="", browser=None, fallback=False):
+        self.tag = tag
+        self.url = url
+        self.description = description
+
+        if browser:
+            self.browser = browser
         else:
-            print("%s\t\t%s" % (i, available_engines[i][2]))
+            self.browser = sengine.default_browser
+
+        # register itself as one of the available engines
+        sengine.tags[self.tag] = self
+
+        # set itself as the fallback engine if explicitly specified
+        if fallback:
+            sengine.fallback = self
+
+
+    def __repr__(self):
+        return f"{self.tag}\t\t{self.description}"
+
+    def instances():
+        return list(sengine.tags.values())
+
+    def print_engines(self):
+        for engine in sengine.instances():
+            print(engine)
+        
+            
 
 ##### Options: #####
-
-browser = "firefox" #insert the command for your preferred browser here.
-fallback_engine = "ddg" #set the fallback engine tag you want to use.
 dmenu_arguments = "" # set additional arguments for dmenu, like font selection or color options.
+sengine.default_browser= "surf"
 
 # Search engines:
-## Insert your custom search engines here following the syntax of the other ones. Don't forget the commas.
-# "engine-tag":["engine url with trailing '?' ", " search prefix ", "name or short description"],
+## Insert your custom search engines here.
 
-available_engines = {
-"ddg":["https://www.duckduckgo.com/?", "q", "DuckDuckGo"],
-"yt":["https://www.youtube.com/results?","search_query", "YouTube"],
-"awiki":["https://wiki.archlinux.org/index.php?", "search", "Arch Wiki"],
-"python2":["https://docs.python.org/2/search.html?", "q", "Python 2.7 documentation"],
-"imgur":["https://imgur.com/search?", "q", "imgur"],
-"wallhaven":["http://alpha.wallhaven.cc/search?", "q", "Wallhaven"],
-"dA":["http://www.deviantart.com/browse/all/?", "section=&global=1&q", "deviantART"],
-"img":["https://duckduckgo.com/?ia=images&iax=1&", "q", "DuckDuckGo image search"],
-}
+sengine(fallback=True,
+       tag="ddg",
+       url="https://www.duckduckgo.com/?q=",
+       description="DuckDuckGo")
+
+sengine(tag="yt",
+       url="https://www.youtube.com/results?search_query=",
+       description="YouTube")
+
+sengine(tag="img",
+       url="https://duckduckgo.com/?ia=images&iax=1&q=",
+       description="DuckDuckGo image search")
 
 ##### End of options. #####
 
 print_only = False
 input_list = sys.argv[1::]
+browser = None
 
 if "--list" in sys.argv[:2:] or "-l" in sys.argv[:2:]:
     list_engines()
@@ -70,34 +102,36 @@ if "--print-only" in sys.argv:
 if "--dmenu" in sys.argv:
     input_list.remove("--dmenu")
     dmenu_tags = ''
-    for tag in available_engines:
-        dmenu_tags += tag + '\n'
-    dmenu_command = "echo \"%s\" | dmenu -p 'Surfpy:' %s" % (dmenu_tags, dmenu_arguments)
+    for engine in sengine.instances():
+        dmenu_tags += engine.tag + '\n'
+    dmenu_command = f"echo \"{dmenu_tags}\" | dmenu -p 'Surfpy:' {dmenu_arguments}"
     
     try:
         input_list = subprocess.check_output(
             dmenu_command,
             stderr=subprocess.STDOUT,
-            shell=True).strip().split()
+            shell=True,
+            text=True).strip().split()
       
     except subprocess.CalledProcessError:
+        print("could not run dmenu!")
         sys.exit(0)
 
-if input_list[0] in available_engines:
-    search_engine = input_list[0]
-    search_string = " ".join(input_list[1::])
-    
-else:
-    search_engine = fallback_engine
-    search_string = " ".join(input_list)
+try:
+    chosen = sengine.tags[input_list[0]];
+    input_list.pop(0)
+except:
+    chosen = sengine.fallback
 
-engine_url = available_engines[search_engine][0]
-search_prefix = available_engines[search_engine][1]
-query_url = engine_url + urlencode({search_prefix:search_string})
+search_string=" ".join(input_list)
+query_url = chosen.url + url_quote(search_string)
+if browser:
+    chosen.browser = browser
 
 if print_only:
     print(query_url)
+
 else:
-    subprocess.Popen([browser, query_url])
+    subprocess.Popen([chosen.browser, query_url])
 
 sys.exit(0)
